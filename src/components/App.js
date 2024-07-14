108d5866034914d24a47b5a0efe8e60df28a537c
Rathansai45 <rathansai45@gmail.com>
2024-07-14 16:41:43 +0530

    solution1

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,42 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+  const [list, setLists] = useState([]);
 
-export default App
+  const addTaskToList = () => {
+    console.log("hello i am inside addtoasktolist");
+    const val = document.getElementById("task").value;
+    if (!val) return;
+    const element = {
+      id: Date.now(),
+      task: val,
+    };
+    setLists([...list, element]);
+  };
+
+  const deleteTask = (id) => {
+    const updatedList = list.filter((list) => list.id !== id);
+    setLists(updatedList);
+  };
+
+  return (
+    <div className="container">
+      <h2>To-Do List</h2>
+      <div className="todo-container">
+        <input id="task" type="text" />
+        <button onClick={addTaskToList}>Add Todo</button>
+        <ul>
+          {list.map((task) => (
+            <li className="list" key={task.id}>
+              {task.task}
+              <button onClick={() => deleteTask(task.id)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default App;
